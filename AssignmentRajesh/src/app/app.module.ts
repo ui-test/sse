567e7c0a67754433ec8574d04aa787667391fe85
rajeshguduguntla <grajesh.1212@gmail.com>
2020-03-05 23:45:24 +0530

    code clean

--- AssignmentRajesh/src/app/app.module.ts
+++ AssignmentRajesh/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MaincomponentComponent } from './maincomponent/maincomponent.component';
 import { BlockcomponentComponent } from './blockcomponent/blockcomponent.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
@@ -11,7 +10,6 @@ import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    MaincomponentComponent,
     BlockcomponentComponent
   ],
   imports: [
